4fbb3114ce134fe73fb21955338e8ebafed31e8a
Miguel Garcia <mgm.mgm.85@gmail.com>
2017-08-05 15:25:36 +0200

    added empty gateway api class

---
+++ inc/GatewayAPI.h
@@ -0,0 +1,17 @@
+/*
+ * GatewayAPI.h
+ *
+ *  Created on: Aug 5, 2017
+ *      Author: migarna
+ */
+
+#ifndef GATEWAYAPI_H_
+#define GATEWAYAPI_H_
+
+class GatewayAPI {
+public:
+	GatewayAPI();
+	virtual ~GatewayAPI();
+};
+
+#endif /* GATEWAYAPI_H_ */
---
+++ src/GatewayAPI.cpp
@@ -0,0 +1,20 @@
+/*
+ * GatewayAPI.cpp
+ *
+ *  Created on: Aug 5, 2017
+ *      Author: migarna
+ */
+
+#include <GatewayAPI.h>
+#include <stdio.h>
+
+GatewayAPI::GatewayAPI() {
+	// TODO Auto-generated constructor stub
+	printf("ola q librerai ase?");
+
+}
+
+GatewayAPI::~GatewayAPI() {
+	// TODO Auto-generated destructor stub
+}
+
